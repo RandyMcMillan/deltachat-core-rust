eeed54d31670b8d5aba3de1bca7e7fd31565f5be
iequidoo <dgreshilov@gmail.com>
2024-11-13 20:06:41 -0300

    fix: Add self-addition message to chat when recreating member list
    A user reported to me that after they left a group, they were implicitly readded, but there's no any
    readdition message, so currently it looks in the chat like leaving it has no effect, just new
    messages continue to arrive. The readdition probably happened because some member didn't receive the
    user's self-removal message, anyway, at least there must be a message that the user is readded, even
    if it isn't known by whom.

--- src/receive_imf.rs
+++ src/receive_imf.rs
@@ -2331,6 +2331,18 @@ async fn apply_group_changes(
                 context,
                 "Recreating chat {chat_id} member list with {new_members:?}.",
             );
+            if !self_added
+                && (chat.blocked == Blocked::Request || !chat_contacts.contains(&ContactId::SELF))
+            {
+                group_changes_msgs.push(
+                    stock_str::msg_add_member_local(
+                        context,
+                        &context.get_primary_self_addr().await?,
+                        ContactId::UNDEFINED,
+                    )
+                    .await,
+                );
+            }
         }
 
         if new_members != chat_contacts {
